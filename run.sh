cf87fe0551b67a33af22c9b093e72a02f242b720
luiz-couto <luiz.felipe.556@outlook.com>
2022-05-14 14:39:16 -0300

    updates

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 rm -r out
 export HADOOP_CP="$(hadoop classpath)"
-javac -cp "${HADOOP_CP}:/home/luizcouto/School/cloud-computing/twitter-analysis/lib/org/*" -d classes src/part_one/TwitterMapReduce.java
+javac --release 8 -cp "${HADOOP_CP}:/home/luizcouto/School/cloud-computing/twitter-analysis/lib/org/*" -d classes src/part_one/TwitterMapReduce.java
 jar -cvf src/part_one/TwitterMapReduce.jar -C classes/ .
 hadoop jar src/part_one/TwitterMapReduce.jar part_one.TwitterMapReduce database/1.json out
